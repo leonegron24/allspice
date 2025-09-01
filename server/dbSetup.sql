0f24cbcb87ccc1ab2ef770a331ab815824b9c5ce
Leonardo Negron <leonegron24@gmail.com>
2025-09-01 15:09:59 -0600

    created some tables and ingredients model

---
+++ server/dbSetup.sql
@@ -0,0 +1,48 @@
+CREATE TABLE IF NOT EXISTS accounts (
+    id VARCHAR(255) NOT NULL PRIMARY KEY COMMENT 'primary key',
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+    name VARCHAR(255) COMMENT 'User Name',
+    email VARCHAR(255) UNIQUE COMMENT 'User Email',
+    picture VARCHAR(255) COMMENT 'User Picture'
+) default charset utf8mb4 COMMENT '';
+
+-- Ingredients
+CREATE TABLE ingredients (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    name VARCHAR(255) NOT NULL,
+    quantity VARCHAR(255) NOT NULL,
+    recipe_id VARCHAR(255) NOT NULL
+)
+
+SELECT * FROM ingredients
+
+-- Recipe
+CREATE TABLE recipes (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    title VARCHAR(255) NOT NULL,
+    instructions VARCHAR(5000),
+    img VARCHAR(1000) NOT NULL,
+    category ENUM(
+        'breakfast',
+        'lunch',
+        'dinner',
+        'snack',
+        'dessert'
+    ) NOT NULL,
+    creator_id VARCHAR(255) NOT NULL,
+    Foreign Key (creator_id) REFERENCES accounts (id) ON DELETE CASCADE
+)
+
+-- Favorite
+CREATE TABLE favorites (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    recipe_id INT NOT NULL,
+    account_id VARCHAR(255) NOT NULL
+)
